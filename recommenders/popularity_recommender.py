29a0c2f1eebee8f40beb705ae5ad991727c7cc3f
Vanraj Modhavadiya <mvanraj901@gmail.com>
2024-01-28 10:30:26 +0530

    task: Task: Build a Popularity Based Recommendation System

--- recommenders/popularity_recommender.py
+++ recommenders/popularity_recommender.py
@@ -1,22 +1,17 @@
 from recommenders import data_movies, data_ratings, default_poster_url
 import pandas as pd
 
-
 def recommend_popular_movies():
 
-    ######################################################################################################
-    movie_ratings = None
-    '''
-    complete the code to compute a variable movie_ratings
-    1. perform a merge between data_movies and data_ratings on 'movieId'.
-    2. group by title and find the mean of ratings and fetch top 20 records.
-    3. convert the records to dict.
-    '''
+    # Merge data_movies and data_ratings
+    merged_data = pd.merge(data_movies, data_ratings, on='movieId')
 
-    #######################################################################################################
+    # Calculate average ratings by grouping by movie title
+    movie_ratings = merged_data.groupby('title')['rating'].mean().sort_values(ascending=False).head(20)
+
     response = []
 
-    for movie in movie_ratings:
+    for movie in movie_ratings.index:
         movie_record = data_movies[data_movies.title == movie].iloc[0]
         movie_poster = None
         response.append({
@@ -27,5 +22,4 @@ def recommend_popular_movies():
             "average_rating": movie_ratings[movie]
         })
 
-
-    return {"status": True, "data": {"message": "Here are some of the popular recommendations.", "results" : response}}
+    return {"status": True, "data": {"message": "Here are some of the popular recommendations.", "results": response}}
